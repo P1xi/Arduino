9a56f64ef2f0d4887a4dd70d88de2f237d9df177
P1xi <94700870+P1xi@users.noreply.github.com>
2021-11-22 15:54:43 +0100

    primera prova (plantilla)

---
+++ Cicle de vida d'un programa/plantilla/plantilla.ino
@@ -0,0 +1,28 @@
+/********************
+*                                                       *
+*                    TÍTOL:                             *
+*                   PLANTILA                            *
+*                                                       *
+* ARNAU PUIGGROS                    DATA:19/11/2022     *
+********************/
+//******* INCLUDE ***********
+
+
+//******* VARIABLES *********
+
+
+//******* SETUP ***********
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+//******** LOOP ***********
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+//******* FUNCIONS **********
