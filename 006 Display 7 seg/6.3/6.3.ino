aec26c95a400d3a59597ed490aa51142300c5f5b
P1xi <94700870+P1xi@users.noreply.github.com>
2021-12-20 16:44:29 +0100

    006 (6.2 + 6.3)

---
+++ 006 Display 7 seg/6.3/6.3.ino
@@ -0,0 +1,197 @@
+/**********************************************************
+**                                                       **
+**                    TÍTOL:                             **
+**                   PLANTILA                            **
+**                                                       **
+** ARNAU PUIGGROS                    DATA:19/11/2022     **
+***********************************************************/
+//******* INCLUDE ************
+
+
+//******* VARIABLES *********
+const byte segA = 2;          // donar nom al pin 5 de l’Arduino
+const byte segB = 3;          
+const byte segC = 4;          
+const byte segD = 5;          
+const byte segE = 6;          
+const byte segF = 7;         
+const byte segG = 8; 
+const byte buttonPin9 = 9;     // donar nom al pin 2 de l’Arduino
+byte buttonState9 = 0;         // per guardar l’estat en que és troba el button2
+int numActual = 0;
+int playpause = 0;
+//******* SETUP ***********
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(segA, OUTPUT);     // definir el pin 5 com una sortida
+  pinMode(segB, OUTPUT);     // definir el pin 6 com una sortida
+  pinMode(segC, OUTPUT);     // definir el pin 7 com una sortida
+  pinMode(segD, OUTPUT);     // definir el pin 8 com una sortida
+  pinMode(segE, OUTPUT);     // definir el pin 9 com una sortida
+  pinMode(segF, OUTPUT);     // definir el pin 10 com una sortida
+  pinMode(segG, OUTPUT);     // definir el pin 11 com una sortida
+  pinMode(buttonPin9, INPUT); // definir el pin 2 com una entrada 
+
+  
+}
+
+//******** LOOP ***********
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  buttonState9 = digitalRead(buttonPin9);
+
+  if (buttonState9 == 0)
+  {
+      playpause = !playpause;
+      delay(200);
+  }
+  if (playpause == 1)
+  {
+      numActual++;
+      if (numActual ==10)
+      {
+        numActual = 0;
+      }
+  }
+  switch (numActual)
+  {
+    case 0:
+      digitalWrite(segA, HIGH);  //0
+      digitalWrite(segB, HIGH);
+      digitalWrite(segC, HIGH);
+      digitalWrite(segD, HIGH);
+      digitalWrite(segE, HIGH);
+      digitalWrite(segF, HIGH);
+      digitalWrite(segG, LOW);
+      
+      delay(500);
+
+      break;
+
+    case 1:
+      digitalWrite(segA, LOW);  //1
+      digitalWrite(segB, HIGH);
+      digitalWrite(segC, HIGH);
+      digitalWrite(segD, LOW);
+      digitalWrite(segE, LOW);
+      digitalWrite(segF, LOW);
+      digitalWrite(segG, LOW);
+    
+      delay(500);
+
+      break;
+
+    case 2:
+      digitalWrite(segA, HIGH);  //2
+      digitalWrite(segB, HIGH);
+      digitalWrite(segC, LOW);
+      digitalWrite(segD, HIGH);
+      digitalWrite(segE, HIGH);
+      digitalWrite(segF, LOW);
+      digitalWrite(segG, HIGH);
+    
+      delay(500);
+
+      break;
+
+    case 3:
+      digitalWrite(segA, HIGH);  //3
+      digitalWrite(segB, HIGH);
+      digitalWrite(segC, HIGH);
+      digitalWrite(segD, HIGH);
+      digitalWrite(segE, LOW);
+      digitalWrite(segF, LOW);
+      digitalWrite(segG, HIGH);
+    
+      delay(500);
+
+      break;
+
+    case 4:
+      digitalWrite(segA, LOW);  //4
+      digitalWrite(segB, HIGH);
+      digitalWrite(segC, HIGH);
+      digitalWrite(segD, LOW);
+      digitalWrite(segE, LOW);
+      digitalWrite(segF, HIGH);
+      digitalWrite(segG, HIGH);
+
+      delay(500);
+
+      break;
+
+    case 5:
+      digitalWrite(segA, HIGH);  //5
+      digitalWrite(segB, LOW);
+      digitalWrite(segC, HIGH);
+      digitalWrite(segD, HIGH);
+      digitalWrite(segE, LOW);
+      digitalWrite(segF, HIGH);
+      digitalWrite(segG, HIGH);
+  
+      delay(500);
+
+
+      break;
+
+    case 6:
+      digitalWrite(segA, LOW);  //6
+      digitalWrite(segB, LOW);
+      digitalWrite(segC, HIGH);
+      digitalWrite(segD, HIGH);
+      digitalWrite(segE, HIGH);
+      digitalWrite(segF, HIGH);
+      digitalWrite(segG, HIGH);
+  
+      delay(500);
+
+      break;
+
+    case 7:
+      digitalWrite(segA, HIGH);  //7
+      digitalWrite(segB, HIGH);
+      digitalWrite(segC, HIGH);
+      digitalWrite(segD, LOW);
+      digitalWrite(segE, LOW);
+      digitalWrite(segF, LOW);
+      digitalWrite(segG, LOW);
+  
+      delay(500);
+
+      break;
+
+    case 8:
+      digitalWrite(segA, HIGH);  //8
+      digitalWrite(segB, HIGH);
+      digitalWrite(segC, HIGH);
+      digitalWrite(segD, HIGH);
+      digitalWrite(segE, HIGH);
+      digitalWrite(segF, HIGH);
+      digitalWrite(segG, HIGH);
+  
+      delay(500);
+
+      break;
+
+    case 9:
+      digitalWrite(segA, HIGH);  //9
+      digitalWrite(segB, HIGH);
+      digitalWrite(segC, HIGH);
+      digitalWrite(segD, LOW);
+      digitalWrite(segE, LOW);
+      digitalWrite(segF, HIGH);
+      digitalWrite(segG, HIGH);
+  
+      delay(500);
+
+      break;
+      
+  }
+}
+
+
+
+//******* FUNCIONS **********
