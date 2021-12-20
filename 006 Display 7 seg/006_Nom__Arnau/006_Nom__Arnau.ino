faa46f11544ddf5316b9f9c169afe6f15ee8e752
P1xi <94700870+P1xi@users.noreply.github.com>
2021-12-20 15:49:16 +0100

    Create 006_Nom__Arnau.ino

---
+++ 006 Display 7 seg/006_Nom__Arnau/006_Nom__Arnau.ino
@@ -0,0 +1,91 @@
+/**********************************************************
+**                                                       **
+**                    TÍTOL:                             **
+**                   PLANTILA                            **
+**                                                       **
+** ARNAU PUIGGROS                    DATA:19/11/2022     **
+***********************************************************/
+//******* INCLUDE ************
+
+
+//******* VARIABLES *********
+const byte segA = 2;          // donar nom al pin 5 de l’Arduino
+const byte segB = 3;          
+const byte segC = 4;          
+const byte segD = 5;          
+const byte segE = 6;          
+const byte segF = 7;         
+const byte segG = 8;  
+
+
+//******* SETUP ***********
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(segA, OUTPUT);     // definir el pin 5 com una sortida
+  pinMode(segB, OUTPUT);     // definir el pin 6 com una sortida
+  pinMode(segC, OUTPUT);     // definir el pin 7 com una sortida
+  pinMode(segD, OUTPUT);     // definir el pin 8 com una sortida
+  pinMode(segE, OUTPUT);     // definir el pin 9 com una sortida
+  pinMode(segF, OUTPUT);     // definir el pin 10 com una sortida
+  pinMode(segG, OUTPUT);     // definir el pin 11 com una sortida
+  
+
+}
+
+//******** LOOP ***********
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, LOW);
+  digitalWrite(segE, HIGH);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, HIGH);
+  
+  delay(500);
+
+  digitalWrite(segA, LOW);
+  digitalWrite(segB, LOW);
+  digitalWrite(segC, LOW);
+  digitalWrite(segD, LOW);
+  digitalWrite(segE, HIGH);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, HIGH);
+
+  delay(500);
+
+  digitalWrite(segA, LOW);
+  digitalWrite(segB, LOW);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, LOW);
+  digitalWrite(segE, HIGH);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, HIGH);
+
+  delay(500);
+
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, LOW);
+  digitalWrite(segE, HIGH);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, HIGH);
+
+  delay(500);
+
+  digitalWrite(segA, LOW);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, HIGH);
+  digitalWrite(segE, HIGH);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, LOW);
+
+  delay(500);
+}
+
+//******* FUNCIONS **********
